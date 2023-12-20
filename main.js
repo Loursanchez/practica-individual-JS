cef2a34bec19971da1350a70b33fd05a4e9c1224
Lourdes S. Recio <lourdess.recio@iMac.home>
2023-12-20 10:38:49 +0100

    subir hasta ejercicio 4

---
+++ main.js
@@ -0,0 +1,25 @@
+// Ejercicio 1 //
+function sumaNumeros(Num1, Num2) {
+    return Num1 + Num2;
+  }
+console.log(sumaNumeros(5, 3)); 
+
+// Ejercicio 2 //
+function restaNumeros (Num1, Num2) {
+  return Num1 - Num2;
+}
+console.log(restaNumeros(5, 3));
+
+// Ejercicio 3 //
+function productoNumeros (Num1, Num2) {
+  return Num1 * Num2;
+}
+console.log(productoNumeros(5, 3));
+
+// Ejercicio 4 //
+function divisionNumeros (Num1, Num2) {
+  return Num1 / Num2
+}
+console.logdivisionNumeros (Num1, Num2) {
+  
+}
